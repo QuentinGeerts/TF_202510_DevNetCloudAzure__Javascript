39d7f393de7604a3b55a15336599cbb7aaaa5df3
Quentin Geerts <quentin.geerts@bstorm.be>
2025-10-21 15:33:53 +0200

    Correction exercice 3

--- 1_algorithmie/exercices/exo03-tableau10-valeurs/script.js
+++ 1_algorithmie/exercices/exo03-tableau10-valeurs/script.js
@@ -8,3 +8,26 @@ Afficher également le tableau formatté.
 Format demandé: "[ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]"
 
 */
+
+const myArray = new Array(10);
+// let somme = 0;
+
+for (let i = 0; i < myArray.length; i++) {
+  let valeur;
+  do {
+    valeur = parseInt(prompt(`Entrez la valeur n°${i + 1}:`));
+  } while (isNaN(valeur));
+
+  myArray[i] = valeur;
+  // somme += valeur;
+}
+
+// console.log('somme :>> ', somme);
+
+let somme = myArray.reduce((acc, value) => acc + value, 0);
+let moyenne = somme / myArray.length;
+let format = `[ ${myArray.join(', ')} ]`;
+
+console.log(`Tableau: ${format}`);
+console.log(`Moyenne: ${moyenne}, somme: ${somme}`);
+
