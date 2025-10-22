2a2eed8d63e9f3ff55b7a3bf794fb1d5e6ac0618
Quentin Geerts <quentin.geerts@bstorm.be>
2025-10-22 14:39:21 +0200

    Énoncé: liste interactive

---
+++ 2_dom/exercices/exo01-liste-interactive/script.js
@@ -0,0 +1,13 @@
+/*
+
+  Créer un programme qui permet d'encoder un libellé et de valider avec un bouton à côté.
+  Les deux éléments doivent se situer à l'intérieur d'un formulaire.
+
+  Lorsque vous validez le formulaire, une puce (•) doit apparaitre dans une liste avec le libellé qui a entré.
+
+  Lorsque le champ a été créé, le formulaire se réinitialise et le champ se refocus.
+
+  Attention, un champs vide n'est pas toléré !
+
+*/
+
