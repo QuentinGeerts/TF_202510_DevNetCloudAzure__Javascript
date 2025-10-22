7ed52407ccf96909788e8955a85102dee2fba01b
Quentin Geerts <quentin.geerts@bstorm.be>
2025-10-22 11:04:20 +0200

    Démonstration 01 - Récupération DOM fin

--- 2_dom/demo01-recuperation/script.js
+++ 2_dom/demo01-recuperation/script.js
@@ -6,6 +6,8 @@
 const b01 = document.getElementById("b01");
 console.log('b01 :>> ', b01);
 
+console.clear();
+
 // 2. Document.getElementsByClassName(classNames: string): HTMLCollectionOf<Element> 
 
 const titles = document.getElementsByClassName("title");
@@ -13,3 +15,18 @@ console.log('titles :>> ', titles);
 console.log('titles[0] :>> ', titles[0]);
 console.log('titles.t01 :>> ', titles.t01);
 console.log('titles["t-01"] :>> ', titles["t-01"]);
+
+console.clear();
+
+// 3. Document.getElementsByTagName(tagName: string): HTMLCollectionOf<HTMLElement>
+
+const paragraphes = document.getElementsByTagName("p");
+console.log('paragraphes :>> ', paragraphes);
+
+console.clear();
+
+// 4.1 ParentNode.querySelector<Element>(selectors: string): Element | null
+// 4.2 ParentNode.querySelectorAll<Element>(selectors: string): NodeListOf<Element> | null
+
+const paragraphesAfterTitle = document.querySelectorAll(".title + p");
+console.log('paragraphesAfterTitle :>> ', paragraphesAfterTitle);
